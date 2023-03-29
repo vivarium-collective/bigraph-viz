28196996d478f10757f9c1e1e8aea596a7209e6f
Eran Agmon <agmon.eran@gmail.com>
2023-03-29 08:51:51 -0400

    setup

--- setup.py
+++ setup.py
@@ -4,44 +4,34 @@ from setuptools import setup, find_packages
 VERSION = '0.0.1'
 
 
-if __name__ == '__main__':
-    with open("README.md", 'r') as readme:
-        description = readme.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-    setup(
-        name='bigraph-viz',
-        version=VERSION,
-        packages=find_packages,
-        author='Eran Agmon',
-        author_email='agmon.eran@gmail.com',
-        url='https://github.com/vivarium-collective/bigraph-viz',
-        project_urls={},
-        license='MIT',
-        entry_points={
-            'console_scripts': []},
-        description='plotting tool for compositional bigraph schema',
-        long_description=description,
-        long_description_content_type='text/markdown',
-        keywords='bigraph multi-scale network visualization',
-        package_data={},
-        include_package_data=True,
-        classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Intended Audience :: Developers',
-            'Intended Audience :: Education',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: MIT License',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
-            'Programming Language :: Python :: 3.10',
-            'Programming Language :: Python :: 3.11',
-            'Topic :: Scientific/Engineering',
-        ],
-        python_requires='>=3.8',
-        install_requires=[
-            'graphviz',
-        ],
-    )
+setup(
+    name="bigraph-viz",
+    version=VERSION,
+    author="Eran Agmon",
+    author_email="agmon.eran@gmail.com",
+    description="A short description of your package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/your_username/your_project",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        # List your package dependencies here
+        "graphviz"
+    ],
+)
