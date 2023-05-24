de6978eeea13feddef3b86cb1d166587b2eba3c0
Eran <agmon.eran@gmail.com>
2023-05-24 14:52:58 -0400

    convert vivarium composites to bigraph for plotting

--- bigraph_viz/__init__.py
+++ bigraph_viz/__init__.py
@@ -1,2 +1,3 @@
 from bigraph_viz.plot import plot_bigraph, plot_flow, plot_multitimestep
 from bigraph_viz.dict_utils import pp, pf, schema_state_to_dict
+from bigraph_viz.convert import convert_vivarium_composite
