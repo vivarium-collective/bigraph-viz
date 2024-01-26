953b67350d6b538feb8f31de5912cd721bb46432
Eran <agmon.eran@gmail.com>
2024-01-26 18:07:59 -0500

    wip

--- bigraph_viz/diagram.py
+++ bigraph_viz/diagram.py
@@ -3,96 +3,66 @@ Bigraph diagram
 """
 import os
 from bigraph_schema import TypeSystem, Edge
-from bigraph_schema.registry import type_schema_keys, function_keys
+# from process_bigraph import core
+from bigraph_viz.plot import (
+    absolute_path, extend_bigraph, get_state_path_extended,
+    check_if_path_in_removed_nodes, make_label,
+)
 import graphviz
 
 
-SCHEMA_KEYS = type_schema_keys | set(function_keys)
+# make a local type system
+core = TypeSystem()
+
+
+SCHEMA_KEYS = ['_type', 'config', 'address']  # get these from bigraph_schema
+PROCESS_INTERFACE_KEYS = ['inputs', 'outputs']
 EDGE_TYPES = ['process', 'step', 'edge']
 
 
-def absolute_path(path, relative):
-    progress = list(path)
-    for step in relative:
-        if step == '..' and len(progress) > 0:
-            progress = progress[:-1]
-        else:
-            progress.append(step)
-    return tuple(progress)
-
-
-def extend_bigraph(bigraph, bigraph2):
-    for key, values in bigraph.items():
-        if isinstance(values, list):
-            bigraph[key] += bigraph2[key]
-        elif isinstance(values, dict):
-            bigraph[key].update(bigraph2[key])
-    return bigraph
-
-
-def check_if_path_in_removed_nodes(path, remove_nodes):
-    if remove_nodes:
-        return any(remove_path == path[:len(remove_path)] for remove_path in remove_nodes)
-    return False
-
-
-def make_label(label):
-    return f'<{label}>'
+def get_flattened_wires(wires, hyperedges):
+    pass
 
 
 def get_flattened_graph(
         schema,
+        state,
+        bigraph=None,
         path=None,
-        remove_nodes=None,
+        # remove_nodes=None,
 ):
     path = path or ()
 
     # initialize bigraph
-    bigraph = {
+    bigraph = bigraph or {
         'state_nodes': [],
         'process_nodes': [],
         'place_edges': [],
-        'hyper_edges': {},
-        'disconnected_hyper_edges': {},
-        'bridges': {},
-        'flow': {},
+        'hyper_edges': [],
+        'disconnected_hyper_edges': [],
+        'bridges': [],
     }
 
-    for key, child in schema.items():
-        path_here = path + (key,)
-        if check_if_path_in_removed_nodes(path_here, remove_nodes):
-            continue  # skip node if path in removed_nodes
-        node = {'path': path_here}
+    for key, value in state.items():
+        subpath = path + (key,)
 
-        if not isinstance(child, dict):
-            # this is a leaf node
-            node['_value'] = child
-            bigraph['state_nodes'] += [node]
-            continue
+        if core.check('edge', value):
+            bigraph['process_nodes'].append({'path': subpath})
 
-        # what kind of node?
-        if child.get('_type') in EDGE_TYPES:
-            # this is a hyperedge/process
-            bigraph['process_nodes'] += [node]
+            # this is an edge, get its inputs and outputs
+            input_wires = value.get('inputs', {})
+            output_wires = value.get('outputs', {})
+
+            for port, input_wire in input_wires.items():
+                bigraph['hyperedges'].append({
+                    'source': subpath,
+                    'target': input_wires,
+                })
+
         else:
-            bigraph['state_nodes'] += [node]
+            bigraph['state_nodes'].append(subpath)
 
-        # check inner states
-        if isinstance(child, dict):
-            # this is a branch node
-            child_bigraph_network = get_flattened_graph(
-                child,
-                path=path_here,
-                remove_nodes=remove_nodes)
-            bigraph = extend_bigraph(bigraph, child_bigraph_network)
 
-            # add place edges to this layer
-            for node in child.keys():
-                child_path = path_here + (node,)
-                if node in SCHEMA_KEYS or check_if_path_in_removed_nodes(child_path, remove_nodes):
-                    continue
-                place_edge = [(path_here, child_path)]
-                bigraph['place_edges'] += place_edge
 
     return bigraph
 
@@ -106,7 +76,6 @@ def get_graphviz_fig(
         dpi='70',
 ):
     """make a graphviz figure from a bigraph_network"""
-
     node_names = []
 
     # node specs
@@ -122,13 +91,6 @@ def get_graphviz_fig(
     graph = graphviz.Digraph(graph_name, engine='dot')
     graph.attr(size=size, overlap='false', rankdir=rankdir, dpi=dpi)
 
-    # check if multiple layers
-    multilayer = False
-    for node in bigraph_network['state_nodes']:
-        node_path = node['path']
-        if len(node_path) > 1:
-            multilayer = True
-
     # state nodes
     graph.attr('node', **state_node_spec)
     for node in bigraph_network['state_nodes']:
@@ -142,12 +104,7 @@ def get_graphviz_fig(
         if schema_label:
             label += schema_label
         label = make_label(label)
-
-        if len(node_path) == 1 and multilayer:
-            # the top node gets a double circle
-            graph.node(node_name, label=label, peripheries='2')
-        else:
-            graph.node(node_name, label=label)
+        graph.node(node_name, label=label)
 
     # process nodes
     process_paths = []
@@ -158,17 +115,7 @@ def get_graphviz_fig(
         node_name = str(node_path)
         node_names.append(node_name)
         label = make_label(node_path[-1])
-
-        # composite processes have sub-nodes
-        composite_process = False
-        for edge in bigraph_network['place_edges']:
-            if edge[0] == node_path:
-                composite_process = True
-        if len(node_path) == 1 and composite_process:
-            # composite processes gets a double box
-            graph.node(node_name, label=label, peripheries='2')
-        else:
-            graph.node(node_name, label=label)
+        graph.node(node_name, label=label)
 
     # place edges
     graph.attr('edge', arrowhead='none', penwidth='2')
@@ -176,7 +123,6 @@ def get_graphviz_fig(
 
         # show edge
         graph.attr('edge', style='filled')
-
         node_name1 = str(edge[0])
         node_name2 = str(edge[1])
         graph.edge(node_name1, node_name2)
@@ -202,7 +148,6 @@ def get_graphviz_fig(
     for node_path, wires in bigraph_network['disconnected_hyper_edges'].items():
         node_name = str(node_path)
         with graph.subgraph(name=node_name) as c:
-            # c.attr(rank='source', rankdir='TB')
             for port, state_path in wires.items():
                 absolute_state_path = absolute_path(node_path, state_path)
                 node_name1 = str(node_path)
@@ -214,14 +159,18 @@ def get_graphviz_fig(
     return graph
 
 
-def plot_diagram(
-        schema,
+def plot_bigraph(
+        state,
+        schema=None,
         out_dir=None,
         filename=None,
         file_format='png',
 ):
+    schema = schema or {}
+    schema = core.infer_schema(schema, state)
+
     # parse out the network
-    bigraph_network = get_flattened_graph(schema)
+    bigraph_network = get_flattened_graph(schema, state)
 
     # make a figure
     graph = get_graphviz_fig(bigraph_network)
@@ -237,21 +186,40 @@ def plot_diagram(
 
 
 def test_diagram_plot():
-    cell_schema = {
+    # metabolic_process_type = {
+    #     '_type': 'process',
+    #     '_inputs': {
+    #         'nutrients': 'any',
+    #     },
+    #     '_outputs': {
+    #         'se'
+    #     }
+    # }
+    # register('metabolic_process', metabolic_process_type)  # TODO -- register where?
+
+
+    cell = {
         'cell': {
             '_type': 'process',
             'config': {},
-            'address': 'local:cell',
-            'inputs': {
+            'address': 'local:cell',   # TODO -- this is where the ports/inputs/outputs come from
+            '_inputs': {
                 'nutrients': 'any',
             },
-            'outputs': {
+            '_outputs': {
                 'secretions': 'any',
                 'biomass': 'any',
             },
+            'inputs': {
+                'nutrients': ['nutrients_store'],
+            },
+            'outputs': {
+                'secretions': ['secretions_store'],
+                'biomass': ['biomass_store'],
+            }
         }
     }
-    plot_diagram(cell_schema, filename='bigraph_cell')
+    plot_bigraph(cell, filename='bigraph_cell')
 
 
 if __name__ == '__main__':
