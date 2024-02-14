4a7f5b3167260e9abbd26771e53e6a28ed82e30c
Eran <agmon.eran@gmail.com>
2024-02-14 17:44:49 -0500

    ignore instance key when plotting. make plot_bigraph the main method

--- bigraph_viz/__init__.py
+++ bigraph_viz/__init__.py
@@ -1,3 +1,4 @@
-from bigraph_viz.plot import plot_bigraph, plot_flow, plot_multitimestep
-from bigraph_viz.dict_utils import pp, pf, schema_state_to_dict
-from bigraph_viz.convert import convert_vivarium_composite
+from bigraph_viz.diagram import plot_bigraph
+# from bigraph_viz.plot import plot_bigraph, plot_flow, plot_multitimestep
+# from bigraph_viz.dict_utils import pp, pf, schema_state_to_dict
+# from bigraph_viz.convert import convert_vivarium_composite
