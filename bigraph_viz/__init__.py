d90674ab4efa2be58d4cdae2c48bc4631bdb0038
Eran <agmon.eran@gmail.com>
2025-02-12 21:45:03 -0500

    get_graph_viz in init

--- bigraph_viz/__init__.py
+++ bigraph_viz/__init__.py
@@ -1,5 +1,5 @@
 import pprint
-from bigraph_viz.visualize_types import VisualizeTypes, plot_bigraph
+from bigraph_viz.visualize_types import VisualizeTypes, plot_bigraph, get_graphviz_fig
 from bigraph_viz.dict_utils import replace_regex_recursive
 
 
